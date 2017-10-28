4ac71186ad34f27577606ee4c219ee4920fabe7e
ajmoo93 <hoganas_1937_70@hotmail.com>
2017-10-28 14:49:16 +0200

    Added some Model classes

---
+++ ProjectTesting2/DataBaseProjekt/Model/Movies.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseProjekt.Model
+{
+    class Movies
+    {
+        public Guid MovieId { get; set; }
+        public string MovieTitle { get; set; }
+        public string Director { get; set; }
+        public int MovieStock { get; set; }
+        public string MovieCategory { get; set; }
+
+    }
+}
