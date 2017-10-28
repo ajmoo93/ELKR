30398985907faefdfea1208343cfb0aee45a0543
Katrina Rosales <katrinarosales@live.com>
2017-10-28 15:52:37 +0200

    Fixed fk in classes.

--- ProjectTesting2/DataBaseProjekt/Model/RentedMovies.cs
+++ ProjectTesting2/DataBaseProjekt/Model/RentedMovies.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DataBaseProjekt.Model
 {
@@ -11,8 +14,9 @@ namespace DataBaseProjekt.Model
         public Guid RentalId { get; set; }
         public DateTime RentalDate { get; set; }
         public DateTime ReturnDate { get; set; }
-
+        [ForeignKey("Movie")]
         public Guid MovieId { get; set; }
+        [ForeignKey("Person")]
         public Guid PersonId { get; set; }
 
         public Person Person { get; set; }
