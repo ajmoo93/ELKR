e612ca1c91e27a90750892f732c378ec3276d7ae
ajmoo93 <hoganas_1937_70@hotmail.com>
2017-11-02 17:10:20 +0100

    Added The DTO classes. for the database

---
+++ ProjectTesting2/DataBaseProjekt/DTO/MovieCategoryGTo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseProjekt.DTO
+{
+    public class MovieCategoryGTo
+    {
+        public Guid CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
---
+++ ProjectTesting2/DataBaseProjekt/DTO/RentedMoviesDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseProjekt.DTO
+{
+    public class RentedMoviesDTO
+    {
+        public Guid RentalId { get; set; }
+        public DateTime RentalDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+    }
+}
