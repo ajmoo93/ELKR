862724362ed23db66237d2f219e30997ccac7f2c
ajmoo93 <hoganas_1937_70@hotmail.com>
2017-11-02 18:19:09 +0100

    Trying IRepository before repository

---
+++ ProjectTesting2/DataBaseProjekt/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using DataBaseProjekt.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseProjekt.Interfaces
+{
+    public interface IRepository
+    {
+        void CreatePerson(PersonDTO dto);
+    }
+}
