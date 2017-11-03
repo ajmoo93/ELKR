77cb6a17a1bd12d910ad9367c974bb21a7d1e338
ajmoo93 <hoganas_1937_70@hotmail.com>
2017-11-03 16:53:40 +0100

    Methods in Repository

--- ProjectTesting2/DataBaseProjekt/Repository/Repository.cs
+++ ProjectTesting2/DataBaseProjekt/Repository/Repository.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using DataBaseProjekt.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataBaseProjekt.DTO;
 
 namespace DataBaseProjekt.Repository
 {
     public class Repository : IRepository
     {
+        public void CreateAccount(AccountDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreatePerson(PersonDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAccount(AccountDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditAccount(AccountDTO dto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
